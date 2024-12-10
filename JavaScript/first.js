23ea902f0168de8f3ce2c25bb9ed8265fa6ab432
Narsi <chnarsimha2580@gmail.com>
2024-12-10 12:31:34 +0530

    javascript is added

---
+++ JavaScript/first.js
@@ -0,0 +1,118 @@
+// Types of variable
+
+/*   
+var = which is used to re-declared and update the value , its a global scope
+   ---> Accroding to the es2015 we dont is use the var keyword.
+   var age = 12
+   var age = 21
+   var age = 97  // every time it will take the new value its not a standar rule
+  
+let = can not re-declared but update the value , block scope variable
+const = can not re-declared and not update the value , block scope variable.
+
+scope = Is a block of code mainly used in functions
+*/
+
+
+// variables declartion
+console.log("Hello Narsimha");
+full_name = "Narsimha Chellaboina";
+is_active = true
+let age = 29
+const information = "Backedn developer"
+console.log(full_name+age+is_active+" "+information)
+let value = 23
+let price = 24.12
+let myname = "Narsimha"
+
+// data type below one's are primitive data types
+let datatype = "Number , string, Boolean, undefined, Null, BigInt, Symbol"
+let person_age = 24
+let school_name = "ZPHS"
+let isAllowed = true
+let x; // undefined
+let grade = null ; // null it self is object that represents the intentional absence of any object value.
+let sum = BigInt("123")
+let marks = Symbol("Hello!")
+
+// Non - Primitive data type are 
+/*  
+Objects (arrays,functions) both will come under objects category.
+Objctes are the collection of values just like an Dictonary 
+{
+age : 24, name:"Narsimha",role:"Backed"
+}
+we use the const & let variable to declare the objects
+arrays
+functions
+
+*/
+
+const student = {
+    FullName: "Narsimha",
+    age: 24,
+    Isworking: true,
+    Income: "10 LPA"
+};
+student["age"] = student["age"] +5
+console.log(student["Income"])
+console.log(student.age)
+
+
+// control statements
+let your_age = 18
+if (your_age > 18){
+    console.log("You can vote")
+}else{
+    console.log("Your not eligible")
+}
+
+
+
+let mode = "dark";
+let color;
+
+if (mode == "dark"){
+    color = "Black"
+}else if(mode == "light"){
+    color = "white"
+}else{
+    color = null
+}
+console.log(color)
+
+let number = 29
+
+if (number%2 == 0){
+    console.log("Even num")
+}else if(number%2 !=0){
+    console.log(
+        "Odd number"
+    )
+}else{
+    console.log("Check number")
+}
+
+
+
+
+// for loops
+for(count = 1 ; count<=5;count++){
+    console.log("Narsimha");
+}
+
+// sum of numbers 1 to n
+let sum_val = 0
+for(let i = 1; i<=5;i++){
+    // sum_val+=i
+    sum_val = sum_val+i
+}
+console.log(sum_val,"*****")
+
+
+// while loop condition at the start of the loop
+let i = 1
+while(i<=5){
+    console.log("i = ",i);
+    i++;
+}
